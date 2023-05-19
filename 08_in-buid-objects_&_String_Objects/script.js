04214d974ff6dd134234848e91c899c29c32dca3
avayyyyyyy <jabhi465@gmail.com>
2023-05-19 17:12:54 +0530

    Update

--- 08_in-buid-objects_&_String_Objects/script.js
+++ 08_in-buid-objects_&_String_Objects/script.js
@@ -8,22 +8,19 @@ console.log(Math.round(5.66));
 
 // String Objects ==>
 
-let name = new String ("Abhi");
+let name = new String("Abhi");
 console.log(typeof name);
 console.log(name);
 console.log(name.length);
-console.log(name.includes('Ab'));// ==> Means that is name contains "Ab" or not !?
-console.log(name.endsWith('hi'));
-console.log(name.toUpperCase());// ==> Converts Whole Into UpperCase.
+console.log(name.includes("Ab")); // ==> Means that is name contains "Ab" or not !?
+console.log(name.endsWith("hi"));
+console.log(name.toUpperCase()); // ==> Converts Whole Into UpperCase.
 console.log(name.toLowerCase()); // ==> Converts Whole Into LowerCase
 console.log(name.trim()); // ==> Trims the extra whitespace from both the sides.
 console.log(name.replace("Jain    ", "Verma")); // ==>
 console.log(name.length);
 console.log(name);
 console.log(name.split(" "));
-
-
-
 
 // Template Literals ==> ` `
 
