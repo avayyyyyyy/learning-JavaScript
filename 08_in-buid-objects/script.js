c12fe9fa1591708e1fd21324639eb0457a109fbb
avayyyyyyy <jabhi465@gmail.com>
2023-05-19 16:55:14 +0530

    Build In Objects

--- 08_in-buid-objects/script.js
+++ 08_in-buid-objects/script.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+// MATH Functon ==> Inbuild Object Functions
+
+console.log(Math.round(1.2));
+console.log(Math.PI);
+console.log(Math.round(5.66));
+
+// String Objects ==>
+
+let name = new String ("Abhi Jain    ");
+console.log(typeof name);
+console.log(name);
+console.log(name.length);
+console.log(name.includes('Ab'));// ==> Means that is name contains "Ab" or not !?
+console.log(name.endsWith('hi'));
+console.log(name.toUpperCase());// ==> Converts Whole Into UpperCase.
+console.log(name.toLowerCase()); // ==> Converts Whole Into LowerCase
+console.log(name.trim()); // ==> Trims the extra whitespace from both the sides.
+console.log(name.replace("Jain    ", "Verma")); // ==>
+console.log(name.length);
+console.log(name);
