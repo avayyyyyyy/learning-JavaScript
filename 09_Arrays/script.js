a4f7ae1dc9ec04380fb80116a704d1aca5646cb4
avayyyyyyy <jabhi465@gmail.com>
2023-05-19 18:20:39 +0530

    Update

--- 09_Arrays/script.js
+++ 09_Arrays/script.js
@@ -1,54 +1,54 @@
-// console.log("Hello World!");
+console.log("Hello World!");
 
-// let numbers = [1, 2, 3, 4, 5];
+let numbers = [1, 2, 3, 4, 5];
 
-// // console.log(numbers[0]);
+// console.log(numbers[0]);
 
-// // Insertin in the array from the end ==> array.push(9)
-// numbers.push(6);
-
-// for (let i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-
-// console.log("**************");
-
-// // POP ==> Refers to delete the last element from an array.
-// numbers.pop();
-
-// for (let i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-
-// console.log("**************");
-
-// // "unshift" ==> it helps to insert the element from the beggining of the array.
-// numbers.unshift(0);
-
-// for (let i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-
-// console.log("**************");
-
-// numbers.splice(3);
-
-// for (let i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-// console.log("**************");
-
-// numbers.push(4, 5, 6);
-// for (let i = 0; i < numbers.length; i++) {
-//   console.log(numbers[i]);
-// }
-
-// console.log("**************");
-
-
-
-numbers.slice(1,1); // ==> 
+// Insertin in the array from the end ==> array.push(9)
+numbers.push(6);
 
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
 }
+
+console.log("**************");
+
+// POP ==> Refers to delete the last element from an array.
+numbers.pop();
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+console.log("**************");
+
+// "unshift" ==> it helps to insert the element from the beggining of the array.
+numbers.unshift(0);
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+console.log("**************");
+
+numbers.splice(3);
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+console.log("**************");
+
+numbers.push(4, 5, 6);
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+console.log("**************");
+
+
+
+console.log(numbers.slice(1,5)); // ==> Slice consists of two numbers --> First one represents the starting point and second represents the preciedence ending point value.
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
