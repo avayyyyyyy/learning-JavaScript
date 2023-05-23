cf20ec2dd226c282a7ca4ebff36577d9ca5e2bcf
avayyyyyyy <jabhi465@gmail.com>
2023-05-24 01:05:41 +0530

    Update

--- 10_Adv_Arrays/script.js
+++ 10_Adv_Arrays/script.js
@@ -1,1 +1,26 @@
-console.log("Hello World!")
+// const names =  ["Abhi","Somya","Komal","Bablu"];
+// ARRAY ForEach
+
+// names.forEach((name, i) => {console.log(name, i)});
+
+// Use When
+// You want to do something with each elementof the array.
+
+// Don't use when 
+// You want to stop or break the loop when some condition is true
+// you're working with async functions
+
+// For Example ==> 
+let sum  = 0 ;
+let numbers=[1,2,3,4,5,6];
+
+numbers.forEach((number)=>{ sum +=number });
+
+console.log(sum);
+
+
+function add (2,2){
+   let result = a+b;
+}
+
+console.log(result);
