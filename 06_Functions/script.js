e9dbce1525896b8f9bc723d8fbb39bf6233cb499
avayyyyyyy <jabhi465@gmail.com>
2023-05-25 20:08:05 +0530

    Update

--- 06_Functions/script.js
+++ 06_Functions/script.js
@@ -118,20 +118,87 @@
 
 // Getter & Setter Functions ==> The set method is used to set the value and the get method is used to get the value which we have setted.
 
-naam = {
-  pName: "Abhi",
-  aName: "Jain",
+// naam = {
+//   pName: "Abhi",
+//   aName: "Jain",
 
-  set fullName(value) {
-    let split = value.split(" ");
-    this.pName = split[0];
-    this.aName = split[1];
-  },
-  get fullName() {
-    return `${this.pName} ${this.aName}`;
-  },
-};
+//   set fullName(value) {
+//     let split = value.split(" ");
+//     this.pName = split[0];
+//     this.aName = split[1];
+//   },
+//   get fullName() {
+//     return `${this.pName} ${this.aName}`;
+//   },
+// };
 
-naam.fullName = "Shubhankit Jain";
+// naam.fullName = "Shubhankit Jain";
 
-console.log(naam.fullName);
+// console.log(naam.fullName);
+
+// let a = [1,2,3,4,5];
+// let total = 0;
+
+// // for (let value of a){
+// //    total += value;
+// // }
+
+// a.reduce() = a.forEach( value => total += value );
+
+// console.log(total);
+
+// let arr = [
+//   {
+//     price: 2,
+//     name: "eggs",
+//   },
+//   {
+//     price: 3,
+//     name: "mayo",
+//   },
+// ];
+
+//  let prices  = arr.map((value) => value.price*100);
+
+//  console.log(prices);
+
+// let numbers = [-1,2,3,5,-9,-7,-2];
+
+// // console.log(numbers.filter((number) => { return number >= 0}).sort());
+
+// let employees = [
+//   { name: "Abhi", salary: 100 },
+//   { name: "Komal", salary: 400 },
+//   { name: "Somya", salary: 500 },
+//   { name: "Yogendra", salary: 1000 },
+// ];
+
+// // let richEmploye = employees.filter(price => price.salary > 300 );
+// // let employeName = employees.map((employe) => employe.name );
+
+// // console.log(employeName);
+
+// console.log(employees.forEach((naam) => console.log(`${naam.name} recived an reward!`)));
+
+// FIND function using startsWith method ==> 
+
+// let states = ["Alaska", "Florida", " Georgia", "California"];
+
+// let startsWithC = states.find((state) => (state.startsWith("C")))
+// console.log(startsWithC);
+
+// let pets  = ["dogs", "cats", "birds"];
+// console.log(pets.includes("dogs"));
+
+// Sorting Algorithm ==> 
+
+// Aescending Ordering
+
+// let numbers = [1,2,3,4,5,6,9,5959,59,5,59,15,16,1,51];
+// console.log(numbers.sort((a,b) => a - b ));
+
+// Descending Ordering
+
+// let numbers = [1,2,3,4,5,6,9,5959,59,5,59,15,16,1,51];
+// console.log(numbers.sort((a,b) => b - a ));
+
