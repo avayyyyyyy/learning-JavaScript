252b88b1deeb4044e74b5205b5089182cfd03954
avayyyyyyy <jabhi465@gmail.com>
2023-05-25 20:36:59 +0530

    Update

--- 10_Adv_Arrays/script.js
+++ 10_Adv_Arrays/script.js
@@ -78,14 +78,100 @@
 
 // MAPING WITH OBJECTS
 
-let random = [5, 6, -5, -1];
+// let random = [5, 6, -5, -1];
 
-// let final  = random.filter(number => number > 0);
+// // let final  = random.filter(number => number > 0);
 
-// console.log(final.map(value => {value : number}));
+// // console.log(final.map(value => {value : number}));
 
-let another = random.filter(number => number > 0).map((number) => {
-  return { value: number };
-});
+// let another = random.filter(number => number > 0).map((number) => {
+//   return { value: number };
+// });
 
-console.log(another);
+// console.log(another);
+
+
+// let a = [1,2,3,4,5];
+// let total = 0;
+
+// // for (let value of a){
+// //    total += value;
+// // }
+
+// a.reduce() = a.forEach( value => total += value );
+
+// console.log(total);
+
+// let arr = [
+//   {
+//     price: 2,
+//     name: "eggs",
+//   },
+//   {
+//     price: 3,
+//     name: "mayo",
+//   },
+// ];
+
+//  let prices  = arr.map((value) => value.price*100);
+
+//  console.log(prices);
+
+// let numbers = [-1,2,3,5,-9,-7,-2];
+
+// // console.log(numbers.filter((number) => { return number >= 0}).sort());
+
+// let employees = [
+//   { name: "Abhi", salary: 100 },
+//   { name: "Komal", salary: 400 },
+//   { name: "Somya", salary: 500 },
+//   { name: "Yogendra", salary: 1000 },
+// ];
+
+// // let richEmploye = employees.filter(price => price.salary > 300 );
+// // let employeName = employees.map((employe) => employe.name );
+
+// // console.log(employeName);
+
+// console.log(employees.forEach((naam) => console.log(`${naam.name} recived an reward!`)));
+
+// FIND function using startsWith method ==>
+
+// let states = ["Alaska", "Florida", " Georgia", "California"];
+
+// let startsWithC = states.find((state) => (state.startsWith("C")))
+// console.log(startsWithC);
+
+// let pets  = ["dogs", "cats", "birds"];
+// console.log(pets.includes("dogs"));
+
+// Sorting Algorithm ==>
+
+// Aescending Ordering
+
+// let numbers = [1,2,3,4,5,6,9,5959,59,5,59,15,16,1,51];
+// console.log(numbers.sort((a,b) => a - b ));
+
+// Descending Ordering
+
+// let numbers = [1,2,3,4,5,6,9,5959,59,5,59,15,16,1,51];
+// console.log(numbers.sort((a,b) => b - a ));
+
+// let array = [1, 2, 3, 4, 5];
+// Array Some ==>
+// console.log(array.some((number) => number > 3)); // Tells that some numbers of this array is greater than 3 ( Atleast one ) ?
+
+// Array Some ==>
+// console.log(array.every((number) => number > 3)); // Tells that every numbers of this array is greater than 0 ( every number should be  )?
+
+// Hardest method of array to understand
+// Array reduce ==>
+
+let groceryList = [29, 12, 45, 35, 87, 110];
+// for (let values in groceryList) {
+//   total = total + values;
+
+let total = 0;
+groceryList.forEach((value) => (total += value));
+
+console.log(total);
