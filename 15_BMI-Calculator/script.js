6372e73d8b2377f7182c0889a96a80be92706869
Shubhankit Jain <111121419+avayyyyyyy@users.noreply.github.com>
2023-07-28 13:01:01 +0000

    Code Optimization

--- 15_BMI-Calculator/script.js
+++ 15_BMI-Calculator/script.js
@@ -3,8 +3,7 @@ const weight = document.getElementById("weight");
 const button = document.getElementById("button");
 
 button.addEventListener("click", () => {
-  //   console.log("hello");
-  button.innerHTML = `${
-    parseInt(weight.value) / parseInt(height.value * height.value)*10000
+  button.innerHTML = `${(
+    parseInt(weight.value) / parseInt(height.value * height.value)*10000).toFixed(2)
   }`;
 });
